bf46f8f1e108330cd66cc893a1b0720394d0ae62
Tim Bezemer <tim@alwaysbelearning.nl>
2023-05-01 19:10:54 +0200

    Small fix to CLI

--- src/test_app/cli.py
+++ src/test_app/cli.py
@@ -12,7 +12,7 @@ def main(verbose):
 
 
 @main.command()
-@click.argument("some_int_param", type=int, default=5)
+@click.option("-i", "--some_int_param", type=int, default=5)
 def run(some_int_param):
     logger.info(f"Printing int '{some_int_param}'")
     print(some_int_param)
